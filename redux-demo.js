c825276e3344096ea98a566df1e7fb189d176662
Daniel Batista <dani_batista20@hotmail.com>
2022-01-27 17:51:04 +0000

    add: store & reducer

--- redux-demo.js
+++ redux-demo.js
@@ -1,6 +1,16 @@
+const redux = require("redux");
+
 // TODO: Store, Reducer
+const counterReducer = (state = { counter: 0 }, action) => {
+  return {
+    counter: state.counter + 1,
+  };
+};
+const store = redux.createStore(counterReducer);
 
 // TODO: Subscription, getState
 
+
 // TODO: Dispatcher, action type
+
 
